91f658465a1ecb7e02899be6dabc43af6e003f97
Ahmad Mursaleen <ahmadmursaleen@gmail.com>
2020-01-19 14:25:58 +0100

    Ascending and descending sorting implemented

--- src/app/views/dashboard/dashboard.component.ts
+++ src/app/views/dashboard/dashboard.component.ts
@@ -8,6 +8,8 @@ import { Subscription } from "rxjs";
 })
 export class DashboardComponent implements OnInit {
   cars: any = {};
+  tempCars: any = []; // This is a variable used for dom manipulation for filtering
+
   // subscription is used to subscribe to an obervable to access async data
   _subscription: Subscription;
   constructor(private router: Router, private clunocars: ClunoCarsService) {}
@@ -27,5 +29,38 @@ export class DashboardComponent implements OnInit {
     this._subscription.unsubscribe();
   }
 
-  orderBy(str: string) {}
+  // Functionalities to sort the car list in ascending and descending order
+  orderBy(str: string) {
+    if (str === "asc") {
+      this.tempCars = Array.of(this.cars);
+      this.cars.Items = this.tempCars[0].Items.sort((obj1, obj2) => {
+        if (
+          parseInt(obj1.pricing.M.price.N) > parseInt(obj2.pricing.M.price.N)
+        ) {
+          return 1;
+        }
+        if (
+          parseInt(obj1.pricing.M.price.N) < parseInt(obj2.pricing.M.price.N)
+        ) {
+          return -1;
+        }
+        return 0;
+      });
+    } else if (str === "dsc") {
+      this.tempCars = Array.of(this.cars);
+      this.cars.Items = this.tempCars[0].Items.sort((obj1, obj2) => {
+        if (
+          parseInt(obj1.pricing.M.price.N) > parseInt(obj2.pricing.M.price.N)
+        ) {
+          return -1;
+        }
+        if (
+          parseInt(obj1.pricing.M.price.N) < parseInt(obj2.pricing.M.price.N)
+        ) {
+          return 1;
+        }
+        return 0;
+      });
+    }
+  }
 }
