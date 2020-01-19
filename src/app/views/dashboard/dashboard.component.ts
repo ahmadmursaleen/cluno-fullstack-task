6a9aefea30ae4a64fa5e351090862979f5ed9504
Ahmad Mursaleen <ahmadmursaleen@gmail.com>
2020-01-19 14:52:28 +0100

    min max dom based functionlity added

--- src/app/views/dashboard/dashboard.component.ts
+++ src/app/views/dashboard/dashboard.component.ts
@@ -10,6 +10,14 @@ export class DashboardComponent implements OnInit {
   cars: any = {};
   tempCars: any = []; // This is a variable used for dom manipulation for filtering
 
+  // togglers to manipulate DOM to toogle between min and max values
+  minToggler: boolean = false;
+  maxToggler: boolean = false;
+
+  // Objects storing details of minimum and maximum priced car
+  maxCar: any = {};
+  minCar: any = {};
+
   // subscription is used to subscribe to an obervable to access async data
   _subscription: Subscription;
   constructor(private router: Router, private clunocars: ClunoCarsService) {}
@@ -31,6 +39,10 @@ export class DashboardComponent implements OnInit {
 
   // Functionalities to sort the car list in ascending and descending order
   orderBy(str: string) {
+    // setting the min and max togglers to false to display the full list of visible cars
+    this.minToggler = false;
+    this.maxToggler = false;
+
     if (str === "asc") {
       this.tempCars = Array.of(this.cars);
       this.cars.Items = this.tempCars[0].Items.sort((obj1, obj2) => {
@@ -62,5 +74,39 @@ export class DashboardComponent implements OnInit {
         return 0;
       });
     }
+  }
+
+  // Fucntionlaity to extract the min value and then úsing that to extact the minimum priced car details
+  getMin() {
+    let min: any;
+    this.maxToggler = false;
+    this.minToggler = true;
+    this.tempCars = Array.of(this.cars);
+    min = Math.min.apply(
+      Math,
+      this.tempCars[0].Items.map(function(o) {
+        return o.pricing.M.price.N;
+      })
+    );
+    this.minCar = this.tempCars[0].Items.find(
+      x => parseInt(x.pricing.M.price.N) == min
+    );
+  }
+
+  // Fucntionlaity to extract the maximum value and then úsing that to extact the maximum priced car details
+  getMax() {
+    let max: any;
+    this.minToggler = false;
+    this.maxToggler = true;
+    this.tempCars = Array.of(this.cars);
+    max = Math.max.apply(
+      Math,
+      this.tempCars[0].Items.map(function(o) {
+        return o.pricing.M.price.N;
+      })
+    );
+    this.maxCar = this.tempCars[0].Items.find(
+      x => parseInt(x.pricing.M.price.N) == max
+    );
   }
 }
